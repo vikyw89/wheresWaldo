657cdca27fe77c694b1957a0f586b04901ce147f
vikyw <vikyw89@gmail.com>
2023-03-30 12:43:49 +0800

    added notif on found or not found connected to firebase added validation checking for coordinate

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -8,51 +8,83 @@ import {
   Zoom,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import { where } from "firebase/firestore";
+import { updateDoc, where } from "firebase/firestore";
 import { FirebaseFirestore } from "firestore-web-wrapper";
 import { Inter } from "next/font/google";
 import Image from "next/image";
-import { useState } from "react";
-import { debugSyncV, updateSyncV, useQueryV, useSyncV } from "use-sync-v";
+import { useEffect, useState } from "react";
+import {
+  debugSyncV,
+  deleteSyncV,
+  updateSyncV,
+  useQueryV,
+  useSyncV,
+} from "use-sync-v";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const defaultState = {
-  wantedList: [
+const validateCoordinate = (playerCLick, playerGuess) => {
+  const { x, y } = playerCLick;
+  const { startX, startY, endX, endY } = playerGuess.coordinate;
+
+  // by elimiation
+  if (startX > x || startY > y || endX < x || endY < y) {
+    updateSyncV("ui.notif.targetCaught", "who's that ??");
+    setTimeout(() => {
+      deleteSyncV("ui.notif.targetCaught");
+    }, 5000);
+    return;
+  }
+
+  // if the guess is right
+  // remove from wantedList data
+  updateSyncV("fetch.wantedList.data", (p) => {
+    const filteredData = p.filter((el) => el.doc_id !== playerGuess.doc_id);
+    return filteredData;
+  });
+
+  // update cloud of times caught
+  delete playerGuess.date_created;
+  FirebaseFirestore.updateDoc(
+    `stages/5TD3C6tj9uIpUyOpxeQd/wanted/${playerGuess.doc_id}`,
     {
-      name: "evilMinion",
-      isFound: false,
-    },
-    {
-      name: "babaYaga",
-      isFound: false,
-    },
-    {
-      name: "bowser",
-      isFound: false,
-    },
-  ],
-  stage: false,
-  paddingTop: 0,
-  cursor: {
-    display: false,
-    x: undefined,
-    y: undefined,
-  },
+      ...playerGuess,
+      times_caught: playerGuess.times_caught + 1,
+    }
+  );
+
+  // notiy user target is caught
+  updateSyncV("ui.notif.targetCaught", `You caught ${playerGuess.name} !`);
+  setTimeout(() => {
+    deleteSyncV("ui.notif.targetCaught");
+  }, 5000);
 };
 
 export default function Home() {
   const theme = useSyncV("theme.dark");
   const ui = useSyncV("ui");
+
   // fetch stage
-  const { data, loading, error } = useQueryV("stages", async () => {
+  const { data, loading, error } = useQueryV("fetch.stages", async () => {
     return await FirebaseFirestore.readCol("stages");
   });
+  const wanted = useQueryV("fetch.wantedList", async () => {
+    const response = await FirebaseFirestore.readCol(
+      "/stages/5TD3C6tj9uIpUyOpxeQd/wanted/"
+    );
+    return response;
+  });
 
-  // debugSyncV("stages.data")
+  const scrollHandler = () => {
+    console.log("scroll");
+    updateSyncV("ui.showTarget", false);
+  };
+
   const clickHandler = (e) => {
     if (ui?.showTarget) {
-      updateSyncV("ui.showTarget", false);
+      updateSyncV("ui.showTarget", (p) => {
+        return !p;
+      });
       return;
     }
     const [x, y] = [e.pageX, e.pageY];
@@ -64,46 +96,22 @@ export default function Home() {
       y: y - window.scrollY,
     });
     updateSyncV("ui.clickCoordinate.image", {
-      x:xRelativeCoordinate,
-      y:yRelativeCoordinate
-    })
+      x: xRelativeCoordinate,
+      y: yRelativeCoordinate,
+    });
     updateSyncV("ui.showTarget", true);
-    debugSyncV("ui.clickCoordinate.image");
-
-    // checkCoordinate();
   };
 
-  const pointerHandler = (e) => {
-    // target frame toggle
-    if (state.cursor.display) {
-      setState((prev) => ({
-        ...state,
-        cursor: {
-          ...prev.cursor,
-          display: false,
-        },
-      }));
-      return;
-    }
+  const validateCatchHandler = (e) => {
+    updateSyncV("ui.showTarget", false);
+    const doc_id = e.target.dataset["doc_id"];
 
-    const [x, y] = [e.pageX, e.pageY];
-    const { height, width } = e.target;
-    const xRelativeCoordinate = Math.floor((x * 100) / width);
-    const yRelativeCoordinate = Math.floor((y * 100) / height);
-    setState((prev) => ({
-      ...prev,
-      cursor: {
-        ...prev.cursor,
-        display: true,
-        x: x - window.scrollX,
-        y: y - window.scrollY,
-      },
-    }));
-    console.log("click");
-  };
+    // get document data for that doc_id
+    const playerGuess = wanted.data.filter((el) => {
+      return el.doc_id === doc_id;
+    })[0];
 
-  const selectWantedHandler = (e) => {
-    console.log(e);
+    validateCoordinate(ui.clickCoordinate.image, playerGuess);
   };
 
   return (
@@ -115,7 +123,6 @@ export default function Home() {
         minHeight: "100%",
         padding: 0,
         margin: 0,
-        overflow: "hidden",
       }}
     >
       <Header />
@@ -126,7 +133,6 @@ export default function Home() {
           width: "100vw",
           display: "flex",
           justifyContent: "center",
-          overflow: "hidden",
           paddingTop: ``,
         }}
       >
@@ -134,9 +140,8 @@ export default function Home() {
           <div
             style={{
               width: "100%",
-              minHeight: "100vh",
-              height: "auto",
-              overflowY: "scroll",
+              minHeight: "100%",
+              // height: "auto",
             }}
           >
             <Image
@@ -172,8 +177,25 @@ export default function Home() {
           </Box>
         )}
       </Box>
-
-      {/* {ui?.showTarget && (
+      {ui?.notif?.targetCaught && (
+        <Typography
+          variant="h6"
+          sx={{
+            color: "red",
+            border: "5px dashed red",
+            borderRadius: "100px",
+            position: "fixed",
+            top: ui.clickCoordinate.screen.y - 50,
+            left: ui.clickCoordinate.screen.x - 50,
+            height: "100px",
+            width: "100px",
+            backdropFilter: "contrast(120%)",
+          }}
+        >
+          {ui.notif.targetCaught}
+        </Typography>
+      )}
+      {ui?.showTarget && (
         <Box>
           <Box
             sx={{
@@ -187,23 +209,39 @@ export default function Home() {
               backdropFilter: "contrast(120%)",
             }}
           ></Box>
-          <Zoom in={ui.clickCoordinate.screen.x}>
+          <Typography
+            variant="h5"
+            sx={{
+              color: "white",
+              position: "fixed",
+              top: ui.clickCoordinate.screen.y - 100,
+              left: ui.clickCoordinate.screen.x - 50,
+            }}
+          >
+            who's this ?
+          </Typography>
+          <Zoom in={true}>
             <ButtonGroup
               orientation="horizontal"
               aria-label="vertical outlined button group"
               sx={{
                 position: "fixed",
-                top: ui.clickCoordinate.screen.y - 25,
-                left: ui.clickCoordinate.screen.x + 50,
+                top: ui.clickCoordinate.screen.y - 50,
+                left: ui.clickCoordinate.screen.x + 75,
                 display: "flex",
                 flexDirection: "column",
               }}
             >
-              {ui.wantedList
-                .filter((el) => el.isFound === false)
-                .map((el, index) => {
+              {wanted?.data &&
+                wanted.data.map((el, index) => {
                   return (
-                    <Button key={index} variant="contained" color="error">
+                    <Button
+                      key={el.doc_id}
+                      variant="contained"
+                      color="error"
+                      onClick={validateCatchHandler}
+                      data-doc_id={el.doc_id}
+                    >
                       {el.name}
                     </Button>
                   );
@@ -211,7 +249,7 @@ export default function Home() {
             </ButtonGroup>
           </Zoom>
         </Box>
-      )} */}
+      )}
 
       <Footer />
     </Box>
